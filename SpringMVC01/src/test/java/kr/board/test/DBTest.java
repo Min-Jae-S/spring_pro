1e0b1cdff9580b13a020de7f486b4694e18e4de3
User <User@DESKTOP-FPDF7SA>
2022-08-12 13:47:22 +0900

    SpringMVC01

---
+++ SpringMVC01/src/test/java/kr/board/test/DBTest.java
@@ -0,0 +1,14 @@
+package kr.board.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DBTest {
+
+	@Test
+	public void testConnection() {
+		fail("Not yet implemented");
+	}
+
+}
